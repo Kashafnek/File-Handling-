be6108d7a720c4b2de938bc694510861d49bf996
Kashafnek <kashafnek17@gmail.com>
2023-11-10 22:54:11 +0500

    FILE HANDLING IN PHP

---
+++ SEERAT.php
@@ -0,0 +1,142 @@
+<?php
+$fileopen = fopen("SEERAT OF HAZRAT MUHAMMAD (Peace Be Upon Him).txt", "r");
+if (!$fileopen) {
+    die("data is failed  ");
+}
+while (!feof($fileopen)) {
+    $line = fgets($fileopen);
+    echo $line . "<br>";
+}
+fclose($fileopen);
+
+
+$fileopen = fopen("SEERAT OF HAZRAT KHADIJA (R.A).txt", "r");
+if (!$fileopen) {
+    die("data is failed  ");
+}
+while (!feof($fileopen)) {
+    $line = fgets($fileopen);
+    echo $line . "<br>";
+}
+fclose($fileopen);
+
+
+$fileopen = fopen("SEERAT OF HAZRAT AYESHA (R.A).txt", "r");
+if (!$fileopen) {
+    die("data is failed  ");
+}
+while (!feof($fileopen)) {
+    $line = fgets($fileopen);
+    echo $line . "<br>";
+}
+fclose($fileopen);
+
+
+$fileopen = fopen("SEERAT OF HAZRAT ABU BAKR (R.A).txt", "r");
+if (!$fileopen) {
+    die("data is failed  ");
+}
+while (!feof($fileopen)) {
+    $line = fgets($fileopen);
+    echo $line . "<br>";
+}
+fclose($fileopen);
+
+
+$fileopen = fopen("SEERAT OF HAZRAT UMAR IBN AL-KHATTAB (R.A).txt", "r");
+if (!$fileopen) {
+    die("data is failed  ");
+}
+while (!feof($fileopen)) {
+    $line = fgets($fileopen);
+    echo $line . "<br>";
+}
+fclose($fileopen);
+
+
+$fileopen = fopen("SEERAT OF HAZRAT USMAN IBN AFFAN (R.A).txt", "r");
+if (!$fileopen) {
+    die("data is failed  ");
+}
+while (!feof($fileopen)) {
+    $line = fgets($fileopen);
+    echo $line . "<br>";
+}
+fclose($fileopen);
+
+$fileopen = fopen("SEERAT OF HAZRAT ALI IBN ABI TALIB (R.A).txt", "r");
+if (!$fileopen) {
+    die("data is failed  ");
+}
+while (!feof($fileopen)) {
+    $line = fgets($fileopen);
+    echo $line . "<br>";
+}
+fclose($fileopen);
+
+
+$fileopen = fopen("SEERAT OF HAZRAT TALHA IBN UBAYDULLAH (R.A).txt", "r");
+if (!$fileopen) {
+    die("data is failed  ");
+}
+while (!feof($fileopen)) {
+    $line = fgets($fileopen);
+    echo $line . "<br>";
+}
+fclose($fileopen);
+
+
+$fileopen = fopen("SEERAT OF HAZRAT ZUBAIR IBN AL-AWWAM (R.A).txt", "r");
+if (!$fileopen) {
+    die("data is failed  ");
+}
+while (!feof($fileopen)) {
+    $line = fgets($fileopen);
+    echo $line . "<br>";
+}
+fclose($fileopen);
+
+
+$fileopen = fopen("SEERAT OF HAZRAT ABDUR REHMAN IBN AWF (R.A).txt", "r");
+if (!$fileopen) {
+    die("data is failed  ");
+}
+while (!feof($fileopen)) {
+    $line = fgets($fileopen);
+    echo $line . "<br>";
+}
+fclose($fileopen);
+
+
+$fileopen = fopen("SEERAT OF HAZRAT SAAD IBN ABI WAQAS (R.A).txt", "r");
+if (!$fileopen) {
+    die("data is failed  ");
+}
+while (!feof($fileopen)) {
+    $line = fgets($fileopen);
+    echo $line . "<br>";
+}
+fclose($fileopen);
+
+
+$fileopen = fopen("SEERAT OF HAZRAT SAEED IBN ZAID (R.A).txt", "r");
+if (!$fileopen) {
+    die("data is failed  ");
+}
+while (!feof($fileopen)) {
+    $line = fgets($fileopen);
+    echo $line . "<br>";
+}
+fclose($fileopen);
+
+
+$fileopen = fopen("SEERAT OF HAZRAT ABU UBAIDAH IBN AL-JARRAH (R.A).txt", "r");
+if (!$fileopen) {
+    die("data is failed  ");
+}
+while (!feof($fileopen)) {
+    $line = fgets($fileopen);
+    echo $line . "<br>";
+}
+fclose($fileopen);
+?>
